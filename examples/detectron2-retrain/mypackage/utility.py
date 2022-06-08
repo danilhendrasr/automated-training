8fd343e14ebdfb89c66bc54c510a7de30b9016d5
hidayat hamir <hidayathamir@gmail.com>
2022-06-08 12:31:00 +0800

    refactor: wrap auto compare & register and place it on mypackage.utility

--- examples/detectron2-retrain/mypackage/utility.py
+++ examples/detectron2-retrain/mypackage/utility.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import mlflow
 
 
 def create_new_version_dir():
@@ -12,6 +13,24 @@ def create_new_version_dir():
     print(f"copy {latest_version_dir} to {new_version_dir}")
     shutil.copytree(latest_version_dir, new_version_dir)
     return new_version_dir
+
+
+def auto_compare_and_register(model, eval_metric, model_name, lower, p_metric):
+    client = mlflow.tracking.MlflowClient()
+    latest_version_run_id = client.get_latest_versions(model_name)[0].run_id
+    latest_p_metric = client.get_metric_history(latest_version_run_id, p_metric)[0].value
+    # Dibawah ini bisa di refactor, sementara ku tulis begini biar jelas.
+    # clear is better than clever
+    if lower is True:
+        if eval_metric > latest_p_metric:
+            mlflow.pytorch.log_model(model, 'model')
+        else:
+            mlflow.pytorch.log_model(model, 'model', registered_model_name=model_name)
+    elif lower is False:
+        if eval_metric > latest_p_metric:
+            mlflow.pytorch.log_model(model, 'model', registered_model_name=model_name)
+        else:
+            mlflow.pytorch.log_model(model, 'model')
 
 # ! to be removed
 # def write_all_versions_benchmark():
