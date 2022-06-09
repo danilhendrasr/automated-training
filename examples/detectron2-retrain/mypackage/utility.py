287937902d65561c3137fbcda000d7fea850dc15
hidayat hamir <hidayathamir@gmail.com>
2022-06-09 11:48:27 +0800

    refactor: lebih mengeneralkan mypackage.utility.auto_compare_and_register. client dan mlflow model dijadikan parameter. \nPenambahan help pada argparse.

--- examples/detectron2-retrain/mypackage/utility.py
+++ examples/detectron2-retrain/mypackage/utility.py
@@ -1,6 +1,5 @@
 import os
 import shutil
-import mlflow
 
 
 def create_new_version_dir():
@@ -15,22 +14,22 @@ def create_new_version_dir():
     return new_version_dir
 
 
-def auto_compare_and_register(model, eval_metric, model_name, lower, p_metric):
-    client = mlflow.tracking.MlflowClient()
+def auto_compare_and_register(model, eval_metric, model_name, lower, p_metric, client, mlflow_model):
+    """Do auto compare eval_metric with latest metric on the latest version model registered"""
     latest_version_run_id = client.get_latest_versions(model_name)[0].run_id
     latest_p_metric = client.get_metric_history(latest_version_run_id, p_metric)[0].value
     # Dibawah ini bisa di refactor, sementara ku tulis begini biar jelas.
     # clear is better than clever
     if lower is True:
         if eval_metric > latest_p_metric:
-            mlflow.pytorch.log_model(model, 'model')
+            mlflow_model.log_model(model, 'model')
         else:
-            mlflow.pytorch.log_model(model, 'model', registered_model_name=model_name)
+            mlflow_model.log_model(model, 'model', registered_model_name=model_name)
     elif lower is False:
         if eval_metric > latest_p_metric:
-            mlflow.pytorch.log_model(model, 'model', registered_model_name=model_name)
+            mlflow_model.log_model(model, 'model', registered_model_name=model_name)
         else:
-            mlflow.pytorch.log_model(model, 'model')
+            mlflow_model.log_model(model, 'model')
 
 # ! to be removed
 # def write_all_versions_benchmark():
